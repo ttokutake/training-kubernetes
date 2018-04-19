e1c9336ce330ff45e57f2ed6a47704eef9509a19
ttokutake <tadatoshi.tokutake@gmail.com>
2018-04-19 16:32:32 +0900

    Done

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Docs
 
 - https://kubernetes.io/docs/tutorials/stateless-application/hello-minikube/
-    - [next](https://kubernetes.io/docs/tutorials/stateless-application/hello-minikube/#create-a-docker-container-image)
 
 # How to create cluster
 
--- hellonode/server.js
+++ hellonode/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const handleRequest = (request, response) => {
   console.log(`Received request for URL: ${request.url}`);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('Hello World Again!');
 };
 const www = http.createServer(handleRequest);
 www.listen(8080);
